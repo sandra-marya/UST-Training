c4da2d87f1b6ceffff5ab37ab4f71c93a43fa4a5
sandra-marya <92135946+sandra-marya@users.noreply.github.com>
2021-12-17 11:36:47 +0530

    Add files via upload

---
+++ Selenium/Sample1.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample1 
+{
+  public static void main(String [] args)
+  {
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\ustjavasdetb423\\Desktop\\chromedriver_win32\\chromedriver.exe");
+	  WebDriver d = new ChromeDriver();
+	  d.navigate().to("http://www.google.com/");
+  }
+}
